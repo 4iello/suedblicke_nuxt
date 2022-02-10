559dae4f0b516e1f3994b96a55b291e5c5865bd4
4iello <alp.aiello@icloud.com>
2022-02-10 20:42:22 +0100

    version 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
+  server: {
+    host: '0.0.0.0',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
